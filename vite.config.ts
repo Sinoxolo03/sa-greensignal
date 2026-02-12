d05216401858de9c6ccf6ad87898b69f24b726f2
Sinoxolo <nzolodaka@gmail.com>
2026-02-13 01:47:33 +0200

    added vercel json and updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -18,4 +18,14 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    minify: "terser",
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 }));
